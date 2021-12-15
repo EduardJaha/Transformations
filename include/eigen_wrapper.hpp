058a02f60579810d4c68e6ce4a6a9f28fe81758d
EduardJaha <jaha.eduard@gmail.com>
2021-12-15 22:34:14 +0100

    adding Eigen::Vector3d

--- include/eigen_wrapper.hpp
+++ include/eigen_wrapper.hpp
@@ -36,23 +36,29 @@ public:
     Eigen::Matrix3d getScale(void);
     Eigen::Vector3d scale(Eigen::Vector3d point_Scale);
 
-    bool setRotate(double degree);                          // done                      
-    Eigen::Matrix3d getRotate(void);
+    bool setRotate(const double &degree);                          // done                      
+    Eigen::Matrix3d getRotate(void) const;
+    Eigen::Vector3d rotate(Eigen::Vector3d point_Rotate);
 
-    bool setShearX(double shearX);                          // done
-    Eigen::Matrix3d getShearX(void);
+    bool setShearX(const double &shearX);                          // done
+    Eigen::Matrix3d getShearX(void) const;
+    Eigen::Vector3d shearX(Eigen::Vector3d point_ShearX);
 
-    bool setShearY(double shearY);                          // done
-    Eigen::Matrix3d getShearY(void);
+    bool setShearY(const double &shearY);                          // done
+    Eigen::Matrix3d getShearY(void) const;
+    Eigen::Vector3d shearY(Eigen::Vector3d point_ShearY);
 
     bool setReflectO();                                     // done
     Eigen::Matrix3d getReflectO(void);
+    Eigen::Vector3d reflectO(Eigen::Vector3d point_ReflectO);
 
     bool setReflectX();                                     // done
-    Eigen::Matrix3d getReflectX(void);          
+    Eigen::Matrix3d getReflectX(void);
+    Eigen::Vector3d reflectX(Eigen::Vector3d point_ReflectX);          
 
     bool setReflectY();                                     // done
     Eigen::Matrix3d getReflectY(void);
+    Eigen::Vector3d reflectY(Eigen::Vector3d point_ReflectY);
 };
 
 #endif
